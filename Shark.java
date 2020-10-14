03645cb8eda33d6ad0e5d90b32ab7053272dd368
sofia-ulya <72841252+sofia-ulya@users.noreply.github.com>
2020-10-14 12:34:31 +0800

    Add files via upload

---
+++ Shark.java
@@ -0,0 +1,14 @@
+public class Shark extends Fish
+{
+	private int sharpTeeth = 50;
+
+	public void Eat()
+	{
+		System.out.println("Eat with " + sharpTeeth + " numbers of teeth");
+	}
+
+	public void SetSharpTeeth(int teeth)
+	{
+		sharpTeeth = teeth;
+	}
+}
